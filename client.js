bcf77c7c28a9f44fe80c58ec2c6ef08accdcd062
jcelik <julius.cc@hotmail.com>
2019-02-22 07:58:33 +0100

    Fixed command line

---
+++ client.js
@@ -0,0 +1,15 @@
+const [
+	IGNORE_NODE,
+	IGNORE_FILE_NAME,
+	min_c_re,
+	min_c_im,
+	max_c_re,
+	max_c_im,
+	max_n,
+	x,
+	y,
+	divisions,
+	...list_of_servers
+] = process.argv
+
+console.log(list_of_servers)
