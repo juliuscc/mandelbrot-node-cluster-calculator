0fed898de512d5feb1bc968b9007648b32d392d3
jcelik <julius.cc@hotmail.com>
2019-02-22 09:53:54 +0100

    Alter dimensions works

--- client.js
+++ client.js
@@ -1,26 +1,37 @@
-// const [
-// 	IGNORE_NODE,
-// 	IGNORE_FILE_NAME,
-// 	min_c_re,
-// 	min_c_im,
-// 	max_c_re,
-// 	max_c_im,
-// 	max_n,
-// 	x,
-// 	y,
-// 	divisions,
-// 	...list_of_servers
-// ] = process.argv
 const fs = require('fs')
+let [
+	IGNORE_NODE,
+	IGNORE_FILE_NAME,
+	min_c_re,
+	min_c_im,
+	max_c_re,
+	max_c_im,
+	max_n,
+	width,
+	height,
+	divisions,
+	...list_of_servers
+] = process.argv
 
-const [width, height] = [1000, 1000]
+min_c_re = parseInt(min_c_re)
+min_c_im = parseInt(min_c_im)
+max_c_re = parseInt(max_c_re)
+max_c_im = parseInt(max_c_im)
+
+max_n = parseInt(max_n)
+width = parseInt(width)
+height = parseInt(height)
 
 const matrix = new Array(height).fill(0).map(() => new Array(width).fill(0))
 
+function pixel_coordinate_to_c_coordinate(pixel, dimension) {
+	return (pixel / dimension - 0.5) * 4
+}
+
 function calculatePixel(pixel_x, pixel_y) {
 	var iterations = 0
-	var cx = -2 + pixel_x / (width / 4)
-	var cy = -2 + pixel_y / (height / 4)
+	var cx = pixel_coordinate_to_c_coordinate(pixel_x, width)
+	var cy = pixel_coordinate_to_c_coordinate(pixel_y, height)
 	var zx = 0
 	var zy = 0
 
@@ -29,7 +40,7 @@ function calculatePixel(pixel_x, pixel_y) {
 		zx = zx * zx - zy * zy + cx
 		zy = 2 * xt + cy
 		iterations++
-	} while (iterations < 2048 && zx * zx + zy * zy < 4)
+	} while (iterations < max_n && zx * zx + zy * zy < 4)
 
 	return iterations
 }
